ac68b99d3079dc4db72f669b1dac83dfbce7aeb3
Richard Hadden <richard.hadden@oeaw.ac.at>
2024-10-01 11:34:40 +0200

    make creation and modification attrs into nodes

--- pangloss/cypher/read.py
+++ pangloss/cypher/read.py
@@ -12,21 +12,26 @@ def build_view_read_query(
     query = f"""
         MATCH path_to_node = (node:{label} {{uuid: $uuid}})
         MATCH (node)-[:PG_CREATED_IN]->(creation:PGCreation)-[:PG_CREATED_BY]->(user:PGUser)
+        
         CALL {{
             OPTIONAL MATCH (node)-[:PG_MODIFIED_IN]->(modification:PGModification)-[:PG_MODIFIED_BY]->(user:PGUser)
             RETURN {{modified_by: user.username, modified_when: modification.modified_when}} as modification
             ORDER BY modification.modified_when 
             LIMIT 1
         }}
+        
         CALL (node) {{
-            OPTIONAL MATCH path_to_direct_nodes = (node)-[]->(:BaseNode)
+            {"OPTIONAL MATCH path_to_direct_nodes = (node)-[]->(:BaseNode)" if model.field_definitions.relation_fields else ""}
             WITH apoc.coll.flatten([
-                collect(path_to_direct_nodes)
+                
+                {"collect(path_to_direct_nodes)," if model.field_definitions.relation_fields else ""}
+                []
             ]) AS paths, node
             CALL apoc.paths.toJsonTree(paths)
                 YIELD value
                 RETURN value as outgoing 
         }}
+        
         RETURN apoc.map.mergeList([node{{.*, created_by: user.username, created_when: creation.created_when}}, outgoing, modification])
     """
     return typing.cast(typing.LiteralString, query), {"uuid": str(uuid)}
