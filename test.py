f26b68a4a74b78f934f878a1aa24c8940a8e183e
Richard Hadden <richard.hadden@oeaw.ac.at>
2025-03-12 13:17:24 +0100

    save

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from typing import Literal
 
 from pydantic import BaseModel
+from ulid import ULID
 
 
 class Person(BaseModel):
     type: Literal["Person"] = "Person"
     name: str
+    id: ULID
 
 
-p = Person.model_validate({"type": "Person", "name": "John"})
+p = Person.model_validate(
+    {"type": "Person", "name": "John", "id": "01JP537KYYEJCXMARMH7CHPFEC"}
+)
 
 print(p)
 print(type(p))
+
+print(p.id)
